cf73405eacd8e2778703ca0892819ebe5beb7869
Ron Boortman <ron@boortman.nl>
2011-12-09 00:52:53 +0100

    Added projects scaffold

---
+++ db/migrate/20111208221714_create_projects.rb
@@ -0,0 +1,18 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.integer :year
+      t.string :car_type
+      t.integer :project_number
+      t.text :description
+      t.text :worked_on
+      t.text :details
+      t.string :image1
+      t.string :image2
+      t.string :image3
+
+      t.timestamps
+    end
+  end
+end
