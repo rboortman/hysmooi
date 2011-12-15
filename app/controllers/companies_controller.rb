ff5ffd2115fb7c730ffaf4ac558265ab6f6dac48
Ron Boortman <ron@boortman.nl>
2011-12-15 01:33:33 +0100

    Integrated projects some more

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,11 @@
 class CompaniesController < OffersController
   def index
+    @vleesbus = Project.find_by_project_number("2011")
+    @blijdorp = Project.find_by_project_number("2041")
+    @meetbus = Project.find_by_project_number("2043")
+    @crepe_bus = Project.find_by_project_number("2047")
+    @manna = Project.find_by_project_number("2063")
+    @remia = Project.find_by_project_number("2069")
   end
 
 end
--- app/controllers/for_sale_controller.rb
+++ app/controllers/for_sale_controller.rb
@@ -1,5 +1,8 @@
 class ForSaleController < OffersController
   def index
+    @pick_up = Project.find_by_project_number("2046")
+    @paardenwagen_2 = Project.find_by_project_number("2055")
+    @diesel = Project.find_by_project_number("2065")
   end
 
 end
