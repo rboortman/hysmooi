46e6a0a186aabd6f358bd72043bf3f90583e0375
Ron Boortman <ron@boortman.nl>
2011-12-18 21:24:11 +0100

    Two more todo points to do

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -1,7 +1,7 @@
 class ConsumersController < OffersController
   def index
     @camphy = Project.find_by_project_number("2010")
-    @cabinedak = Project.find_by_project_number("2041")
+    @cabinedak = Project.find_by_project_number("2012")
     @neusplaten = Project.find_by_project_number("2015")
     @politie_bus = Project.find_by_project_number("2024")
     @type_h = Project.find_by_project_number("2027")
